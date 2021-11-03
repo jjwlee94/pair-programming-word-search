664d4fc4ed2c18703e92c9abaa60a6f90c99cc18
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-03 21:11:42 +0000

    Complete code

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const reverse = []
+
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
     }
+
+    for (l of letters) {
+        reverse.push(l.reverse());
+    }
+
+    for (l of reverse) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
